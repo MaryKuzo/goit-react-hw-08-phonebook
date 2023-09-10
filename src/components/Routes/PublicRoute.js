06857f7e1c3038aceb202a3c30ff2a8a2d777771
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-09-10 08:37:30 +0300

    Update PublicRoute.js

--- src/components/Routes/PublicRoute.js
+++ src/components/Routes/PublicRoute.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Navigate, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import authSelectors from "redux/auth/auth-selectors";
 
 export default function PublicRoute({
